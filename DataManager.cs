d23699f76d664804bb27b7ebd82f2c1738ec3a5d
mahboobeh <mahboobeh.khaleghi1996@gmail.com>
2019-12-30 13:50:35 +0330

    added c#

---
+++ DataManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+     public class DataManager
+    {
+        private string data;
+        private static DataManager instance=null;
+        public static DataManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance=new DataManager();
+                return instance;
+                    }
+        }
+        public string GetData()
+        {
+            return data;
+            
+        }
+        private  DataManager() {
+            data = "hello";
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = DataManager.Instance.GetData();
+            Console.WriteLine(data);
+            Console.ReadKey();
+
+        }
+    }
+}
